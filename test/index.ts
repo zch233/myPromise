9a9093f5cae2c05c295d68fd9644cc59d45cd05c
zhangchao <529743595@qq.com>
2019-12-05 22:00:03 +0800

    init

---
+++ test/index.ts
@@ -0,0 +1,13 @@
+import * as chai from 'chai'
+// import sinon from 'sinon'
+// import sinonChai from 'sinon-chai'
+
+// chai.use(sinonChai)
+
+const assert = chai.assert
+
+describe('myPromise', () => {
+  it('是一个函数', () => {
+    assert(1 === 1)
+  })
+})
