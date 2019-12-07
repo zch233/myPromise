cef70f93a8a40fd7fe468632d118e3b336348cd3
zhangchao <529743595@qq.com>
2019-12-07 20:42:15 +0800

    fix describe

--- test/index.ts
+++ test/index.ts
@@ -11,7 +11,7 @@ describe('myPromise', () => {
   it('是一个函数', () => {
     assert.isFunction(myPromise)
   })
-  it('只允许接受一个函数当参数，并会调用这个函数', () => {
+  it('只允许接受一个函数当参数，并会立即调用这个函数', () => {
     const called = sinon.fake()
     new myPromise(called)
     assert(called)
