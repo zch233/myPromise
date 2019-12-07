88653640c51f628a3f55b2571f85b4bc97716acc
zhangchao <529743595@qq.com>
2019-12-07 21:02:01 +0800

    接受的函数有两个参数，分别是resolve和reject

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export default function myPromise(fn: Function) {
   if (!isFunction(fn)) throw Error('必须传入函数')
-  fn.call(this)
+  this.resolve = () => {}
+  this.reject = () => {}
+  fn.call(undefined, this.resolve, this.reject)
 }
 
 function isFunction(fn:any) {
