f6632755fa2920d44cbd513d023e65656dbd1e46
zhangchao <529743595@qq.com>
2019-12-07 20:56:48 +0800

    fix test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 export default function myPromise(fn: Function) {
+  if (!isFunction(fn)) throw Error('必须传入函数')
   fn.call(this)
+}
+
+function isFunction(fn:any) {
+  if (typeof fn !== 'function') return false
+  return true
 }
 
 myPromise.prototype.then = () => {}
--- test/index.ts
+++ test/index.ts
@@ -11,7 +11,15 @@ describe('myPromise', () => {
   it('是一个函数', () => {
     assert.isFunction(myPromise)
   })
-  it('只允许接受一个函数当参数，并会立即调用这个函数', () => {
+  it('只允许接受一个函数当参数', () => {
+    const promise = new myPromise(() => {})
+    assert(promise)
+    assert.throw(() => {
+      // @ts-ignore
+      new myPromise(1)
+    })
+  })
+  it('接受函数并会立即调用这个函数', () => {
     const called = sinon.fake()
     new myPromise(called)
     assert(called)
@@ -19,5 +27,11 @@ describe('myPromise', () => {
   it('拥有一个then方法', () => {
     const promise = new myPromise(() => {})
     assert.isFunction(promise.then)
+  })
+  it('接受的函数有两个参数，分别是resolve和reject', () => {
+    new myPromise((resolve, reject) => {
+      assert.isFunction(resolve)
+      assert.isFunction(reject)
+    })
   })
 })
